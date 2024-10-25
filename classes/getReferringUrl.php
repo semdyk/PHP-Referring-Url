badfd595444b3d6190d093975b08b5ed610100a7
Sem D. <118135263+semdyk@users.noreply.github.com>
2024-10-25 16:44:55 +0200

    Update getReferringUrl.php

--- classes/getReferringUrl.php
+++ classes/getReferringUrl.php
@@ -4,7 +4,7 @@ if (!empty($_GET['ref_url'])) {
     $referringUrl = $_GET['ref_url'];
 
     if (!filter_var($referringUrl, FILTER_VALIDATE_URL)) {
-        die('Invalid referring URL format.');
+        exit;
     }
 
     header("Location: $referringUrl");
