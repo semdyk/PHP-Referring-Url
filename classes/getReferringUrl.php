8e6e6b1b9a97423b1bfbcce2ec229b35a3a70bcf
Sem D. <118135263+semdyk@users.noreply.github.com>
2024-10-25 17:01:13 +0200

    Update

--- classes/getReferringUrl.php
+++ classes/getReferringUrl.php
@@ -1,12 +1,16 @@
 <?php
 
+// Check if ref_url is empty, if not continue.
 if (!empty($_GET['ref_url'])) {
+    // Set the referring URL from the query parameter.
     $referringUrl = $_GET['ref_url'];
 
+    // Validate the referring URL format using filter_var() function.
     if (!filter_var($referringUrl, FILTER_VALIDATE_URL)) {
         die('Invalid referring URL format.');
     }
 
+    // Redirect the user to the referring URL.
     header("Location: $referringUrl");
     exit;
 }
